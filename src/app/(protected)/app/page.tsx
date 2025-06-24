a2527609c7e133fdde994ee046dee1c10cd8121d
JA <joelarias@protonmail.com>
2025-06-24 13:22:34 -0700

    connected db

--- src/app/(protected)/app/page.tsx
+++ src/app/(protected)/app/page.tsx
@@ -1,4 +1,9 @@
-export default function App() {
+import { prisma } from "@/lib/prisma";
+
+export default async function App() {
+  const users = await prisma.users.findMany({});
+  console.log("USERS", users);
+
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       App Stuff and things go here.
